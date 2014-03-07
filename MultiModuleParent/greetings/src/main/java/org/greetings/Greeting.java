9b98bea2d1e17905a070978d37b5e936a51159f1
rpz <RahulP@sfwltd.co.uk>
2014-03-07 16:50:05 +0530

    Sample Multimodule Project added

---
+++ MultiModuleParent/greetings/src/main/java/org/greetings/Greeting.java
@@ -0,0 +1,9 @@
+package org.greetings;
+
+public class Greeting {
+
+	public String sayHello(String name) {
+
+		return "Hello " + name + " !!!";
+	}
+}
---
+++ MultiModuleParent/greetingsMain/src/main/java/org/greetingsMain/App.java
@@ -0,0 +1,15 @@
+package org.greetingsMain;
+
+import org.greetings.Greeting;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		Greeting greeting = new Greeting();
+
+		System.out.println(greeting.sayHello("Rahul Parikh"));
+	}
+}
